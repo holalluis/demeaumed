80c67fa4e6f9d9105767be040cc07b850349d10f
lbosch <lbosch@icra.cat>
2016-04-25 18:55:12 +0100

    disseny una mica

--- index.php
+++ index.php
@@ -3,62 +3,76 @@
 	<title>demEAUmed</title>
 	<style>
 		*{margin:0}
+		body{font-family:Calibri}
 		table{border-collapse:collapse}
-		th,td{border:1px solid #ccc}
+		th,td{
+			border:1px solid #ccc
+		}
 
 		div{
 			background:#fafafa;
-			border:1px solid black;
 		}
-
 		div.container{ 
 			display:inline-block;
 		}
-		div.column{
+		div.inline{
 			display:inline-block;
 			vertical-align:top;
+			margin-right:-5px;
 		}
-		div.cell{ 
-			padding:1em;
-			margin:-1px;
-		}
-		div.clearfix{clear:both}
 	</style>
-	<script>
-		function toggleDisplay(element)
-		{
-			var display = element.style.display=='none' ? '' : 'none';
-			element.style.display=display;
-		}
-	</script>
 </head><center><body>
 
-<!--main menu-->
-<div>
-	<h1>demEAUmed</h1>
-	Main menu
-</div>
+<!--main menu--><div><h1>demEAUmed</h1>Main menu</div><br>
 
-<br>
-
-<!--content-->
+<!--container-->
 <div class=container>
-	<!--left-->
-	<div class=column>
-		<div class=column>
-			<div class=cell>Basic hotel info</div>
-			<div class=cell>Laundry</div>
-		</div>
-		<div class=column>
-			<div class=cell>Room</div>
-			<div class=cell>Garden</div>
-		</div>
-		<div class=cell style=background:#4bacc6;color:white> Pool </div>
+
+	<!--left column-->
+	<div class=inline>
+		<table id="t1">
+			<style>
+				table#t1 td{
+					text-align:center;
+					padding:4em;
+					border: 2px solid black;
+				}
+			</style>
+			<tr>
+				<td style="background:#4F81BD;color:white">Basic Hotel Information
+				<td style="background:#9BBB59;color:white">Room appliances
+			<tr>
+				<td style="background:#8064A2;color:white">Laundry
+				<td style="background:#F79646;color:white">Garden
+			<tr><td style="background:#4bacc6;color:white" colspan=2>Pool
+		</table>
 	</div>
 
-	<!--right-->
-	<div class=column>
-		<div class=cell>
+	<!--right column-->
+	<div class=inline>
+		<div id=reuse>
+			<style>
+				div#reuse {
+					padding:2em;
+					background:#FFBE86;
+					border:2px solid black;
+				}
+				div#reuse td, div#reuse th{
+					padding:.5em;
+				}
+				div#reuse tr:nth-child(even) 
+				{
+					background:#eee;
+				}
+				div#reuse tr:nth-child(odd) 
+				{
+					background:white;
+				}
+				div#reuse th{
+					background:black;
+					color:white;
+				}
+			</style>
 			Reuse table
 			<table>
 				<tr><th>    <th>App1<th>App2<th>App3
@@ -66,10 +80,23 @@
 				<tr><th>App2<td>    <td>    <td>
 				<tr><th>App3<td>    <td>    <td>
 			</table>
-			<button>Create table</button>
+			<button style="
+				margin:1em;
+				padding:1em;
+			">Create table</button>
 		</div>
-		<div class=cell>
-			kitchen
+		<div id=kitchen>
+			<style>
+				div#kitchen{
+					padding:6em;
+					background:#bcbcbc;
+					color:white;
+					border:2px solid black;
+					margin-top:-2px;
+				}
+			</style>
+			Kitchen
 		</div>
 	</div>
+
 </div>
