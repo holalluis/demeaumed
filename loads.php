a617d9ab80c72a0a00fc7dbd1292529e69149259
lbosch <lbosch@icra.cat>
2017-01-15 20:56:50 +0100

    few upgrades

---
+++ loads.php
@@ -0,0 +1,9 @@
+<!doctype html><html><head><?php include'imports.php'?>
+
+</head><body>
+<!--navbar--><?php include'navbar.php'?>
+<!--title--><div class=title>4. Solve Loads: <span class=subtitle>Contaminants</span></div>
+
+<pre style=margin:1em>
+	Loads will appear here (work in progress)
+</pre>
