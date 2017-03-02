a178ad516336aa83cd97e2711004089871d3480a
lbosch <lbosch@icra.cat>
2017-03-02 18:32:55 +0000

    computing loads work in progress

---
+++ fullScreen.php
@@ -0,0 +1,9 @@
+<!doctype html><html><head>
+
+<?php include'imports.php'?>
+
+</head><body onload=init()>
+
+<!--navbar--><?php include'navbar.php'?>
+
+<!--graph--> <div style="text-align:center;border:1px solid #ccc"> <?php include'graph.php'?> </div>
